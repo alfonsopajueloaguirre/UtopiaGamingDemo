f7e9f2e2e4ac2406a01afff4026cdef0aaac861d
Daniel <Danielmg_21@hotmail.es>
2020-03-23 18:18:44 +0100

    alfonso

---
+++ web_ejer5/src/main/java/es/ipo/app/bibliotecaController.java
@@ -0,0 +1,15 @@
+package es.ipo.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class bibliotecaController {
+	
+	@GetMapping(value= {"biblioteca"})
+	public String serveBiblio(Model model) {
+		return "biblioteca";
+	}
+}
+
